18616a02c06cc231fd5cc2d90b2f3894c15bd12a
Evan <evan.rypel@gmail.com>
2018-10-19 13:24:21 -0700

    Hibernate works on a very basic level

--- src/com/javawebapp/objects/User.java
+++ src/com/javawebapp/objects/User.java
@@ -1,16 +1,58 @@
 package com.javawebapp.objects;
 
+import javax.persistence.*;
+
+import com.javawebapp.util.JavaWebAppUtils;
+
+@Entity
+@Table(name="USER")
 public class User 
 {
+	@Column(name="USERNAME")
 	private String username;
+	
+	@Id
+	@Column(name="ID")
 	private long ID;
+	
+	@Column(name="EMAIL")
 	private String email;
+	
+	@Column(name="PASSWORD")
 	private String password;
 	
-	User(String username, String email, String password) 
+	public User() {
+		
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public long getID() {
+		return ID;
+	}
+
+	public void setID(long iD) {
+		ID = iD;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public User(String username, String email, String password) 
 	{
 		this.username = username;
-		this.ID = 1L;
+		this.ID = JavaWebAppUtils.generateUniqueId();
 		this.email = email;
 		this.password = password;
 	}
