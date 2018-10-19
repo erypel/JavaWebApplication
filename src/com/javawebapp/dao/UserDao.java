01b130f89549dcdf83add43879413bfb512d9c84
Evan <evan.rypel@gmail.com>
2018-10-19 10:49:31 -0700

    some basic textual changes. going to trying installing Hibernate next, so wanted to commit

--- src/com/javawebapp/dao/UserDao.java
+++ src/com/javawebapp/dao/UserDao.java
@@ -5,17 +5,27 @@ import java.util.List;
 import com.javawebapp.objects.User;
 
 public interface UserDao {
-	//TODO potential security flaw with passwords?
 	public List<User> getAllUsers();
+
 	public User getUser(long id);
-	public User getUser(String username);
-	public void deleteUser(String username);
-	public void deleteUser(long id);
-	public void updateUsername(String oldUsername, String newUsername);
-	public void updateUsername(long id, String newUsername);
-	public void updateUserPassword(String username, String newPassword);
-	public void updateUserPassword(long id, String newPassword);
+
+	public User getUser(String userName);
+
+	public User getUser(String userName, String password);
+
 	public void insertUser(String username, String password, String email, Long id);
-	public void insertUser(String username, String pwd, String email);
-	public User getUser(String username, String password);
+
+	public void insertUser(String username, String password, String email);
+
+	public void updateUsername(String oldUserName, String newUserName);
+
+	public void updateUsername(long id, String newUserName);
+
+	public void updateUserPassword(String userName, String newPassword);
+
+	public void updateUserPassword(long id, String newPassword);
+
+	public void deleteUser(String userName);
+
+	public void deleteUser(long id);
 }
