34f155213317b75e9a632b1fa416e655930eeb0a
Evan <evan.rypel@gmail.com>
2018-10-18 17:03:33 -0700

    Added more Cookies filter. need to phase out and combine with old authentication filter

--- src/com/javawebapp/servlet/filters/AuthenticationFilter.java
+++ src/com/javawebapp/servlet/filters/AuthenticationFilter.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+//TODO phase out
 @WebFilter("/AuthenticationFilter")
 public class AuthenticationFilter implements Filter{
 
