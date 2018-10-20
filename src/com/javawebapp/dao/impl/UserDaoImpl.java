ba1b75e78944371a63b4ea6a1e5c41adf5905e15
Evan <evan.rypel@gmail.com>
2018-10-20 14:16:43 -0700

    was playing with the formatter

--- src/com/javawebapp/dao/impl/UserDaoImpl.java
+++ src/com/javawebapp/dao/impl/UserDaoImpl.java
@@ -1,5 +1,6 @@
 package com.javawebapp.dao.impl;
 
+import java.util.Collections;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -17,40 +18,78 @@ import com.javawebapp.hibernate.HibernateUtil;
 import com.javawebapp.objects.User;
 import com.javawebapp.util.JavaWebAppUtils;
 
-public class UserDaoImpl implements UserDao {
-
+public class UserDaoImpl implements UserDao
+{
+	// column names
+	private static final String PASSWORD = "password";
+	private static final String USERNAME = "userName";
+	
 	@Override
-	public List<User> getAllUsers() {
+	public List<User> getAllUsers()
+	{
 		Session session = HibernateUtil.getSessionFactory().openSession();
-		CriteriaBuilder cb = session.getCriteriaBuilder();
-		CriteriaQuery<User> cq = cb.createQuery(User.class);
-		Root<User> root = cq.from(User.class);
-		cq.select(root);
-		Query<User> query = session.createQuery(cq); 
-		List<User> results = query.getResultList();
-		session.close();
+		List<User> results = Collections.emptyList();
+		try
+		{
+			session.beginTransaction();
+			CriteriaBuilder cb = session.getCriteriaBuilder();
+			CriteriaQuery<User> cq = cb.createQuery(User.class);
+			Root<User> root = cq.from(User.class);
+			cq.select(root);
+			Query<User> query = session.createQuery(cq);
+			results = query.getResultList();
+			session.getTransaction().commit();
+		}
+		catch(Exception e)
+		{
+			if(session.getTransaction() != null)
+				session.getTransaction().rollback();
+			e.printStackTrace();
+		}
+		finally
+		{
+			session.close();
+		}
 		return results;
 		
 	}
-
+	
 	@Override
-	public User getUser(long id) {
+	public User getUser(long id)
+	{
 		EntityManager em = HibernateUtil.getUserEntityManagerFactory().createEntityManager();
 		User user = em.find(User.class, id);
 		em.close();
 		return user;
 	}
-
+	
 	@Override
-	public User getUser(String userName) {
+	public User getUser(String userName)
+	{
 		Session session = HibernateUtil.getSessionFactory().openSession();
-		CriteriaBuilder cb = session.getCriteriaBuilder();
-		CriteriaQuery<User> cq = cb.createQuery(User.class);
-		Root<User> root = cq.from(User.class);
-		cq.select(root).where(cb.equal(root.get("userName"), userName));
-		Query<User> query = session.createQuery(cq);
-		List<User> results = query.getResultList();
-		session.close();
+		List<User> results = Collections.emptyList();
+		try
+		{
+			session.beginTransaction();
+			CriteriaBuilder cb = session.getCriteriaBuilder();
+			CriteriaQuery<User> cq = cb.createQuery(User.class);
+			Root<User> root = cq.from(User.class);
+			cq.select(root).where(cb.equal(root.get(USERNAME), userName));
+			Query<User> query = session.createQuery(cq);
+			results = query.getResultList();
+			session.getTransaction().commit();
+		}
+		catch(Exception e)
+		{
+			if(session.getTransaction() != null)
+				session.getTransaction().rollback();
+			e.printStackTrace();
+		}
+		finally
+		{
+			session.close();
+		}
+		
 		if(results.isEmpty())
 			return null;
 		else
@@ -58,36 +97,68 @@ public class UserDaoImpl implements UserDao {
 	}
 	
 	@Override
-	public User getUser(String userName, String password) {
+	public User getUser(String userName, String password)
+	{
 		Session session = HibernateUtil.getSessionFactory().openSession();
-		session.beginTransaction();
-		CriteriaBuilder cb = session.getCriteriaBuilder();
-		CriteriaQuery<User> cq = cb.createQuery(User.class);
-		Root<User> root = cq.from(User.class);
-		cq.select(root).where(cb.and(cb.equal(root.get("userName"), userName), cb.equal(root.get("password"), password)));
-		Query<User> query = session.createQuery(cq);
-		List<User> results = query.getResultList();
-		session.close();
+		List<User> results = Collections.emptyList();
+		try
+		{
+			session.beginTransaction();
+			CriteriaBuilder cb = session.getCriteriaBuilder();
+			CriteriaQuery<User> cq = cb.createQuery(User.class);
+			Root<User> root = cq.from(User.class);
+			cq.select(root)
+					.where(cb.and(cb.equal(root.get(USERNAME), userName), cb.equal(root.get("password"), password)));
+			Query<User> query = session.createQuery(cq);
+			results = query.getResultList();
+			session.getTransaction().commit();
+		}
+		catch(Exception e)
+		{
+			if(session.getTransaction() != null)
+				session.getTransaction().rollback();
+			e.printStackTrace();
+		}
+		finally
+		{
+			session.close();
+		}
+		
 		if(results.isEmpty())
 			return null;
 		else
 			return results.get(0);
 	}
-
+	
 	@Override
-	public void updateUsername(String oldUserName, String newUserName) {
+	public void updateUsername(String oldUserName, String newUserName)
+	{
 		Session session = HibernateUtil.getSessionFactory().openSession();
-		session.beginTransaction();
-		CriteriaBuilder cb = session.getCriteriaBuilder();
-		CriteriaUpdate<User> update = cb.createCriteriaUpdate(User.class);
-		Root<User> root = update.from(User.class);
-		update.set(root.get("userName"), newUserName).where(cb.equal(root.get("userName"), oldUserName));
-		session.createQuery(update).executeUpdate();
-		session.close();
+		try
+		{
+			session.beginTransaction();
+			CriteriaBuilder cb = session.getCriteriaBuilder();
+			CriteriaUpdate<User> update = cb.createCriteriaUpdate(User.class);
+			Root<User> root = update.from(User.class);
+			update.set(root.get(USERNAME), newUserName).where(cb.equal(root.get("userName"), oldUserName));
+			session.createQuery(update).executeUpdate();
+			session.getTransaction().commit();
+		}
+		catch(Exception e)
+		{
+			if(session.getTransaction() != null)
+				session.getTransaction().rollback();
+			e.printStackTrace();
+		}
+		finally
+		{
+			session.close();
+		}
 	}
-
+	
 	@Override
-	public void updateUsername(long id, String newUserName) {
+	public void updateUsername(long id, String newUserName)
+	{
 		EntityManager em = HibernateUtil.getUserEntityManagerFactory().createEntityManager();
 		User user = em.find(User.class, id);
 		em.getTransaction().begin();
@@ -95,21 +166,36 @@ public class UserDaoImpl implements UserDao {
 		em.getTransaction().commit();
 		em.close();
 	}
-
+	
 	@Override
-	public void deleteUser(String userName) {
+	public void deleteUser(String userName)
+	{
 		Session session = HibernateUtil.getSessionFactory().openSession();
-		session.beginTransaction();
-		CriteriaBuilder cb = session.getCriteriaBuilder();
-		CriteriaDelete<User> cd = cb.createCriteriaDelete(User.class);
-		Root<User> root = cd.from(User.class);
-		cd.where(cb.equal(root.get("userName"), userName));
-		session.createQuery(cd).executeUpdate();
-		session.close();
+		try
+		{
+			session.beginTransaction();
+			CriteriaBuilder cb = session.getCriteriaBuilder();
+			CriteriaDelete<User> cd = cb.createCriteriaDelete(User.class);
+			Root<User> root = cd.from(User.class);
+			cd.where(cb.equal(root.get(USERNAME), userName));
+			session.createQuery(cd).executeUpdate();
+			session.getTransaction().commit();
+		}
+		catch(Exception e)
+		{
+			if(session.getTransaction() != null)
+				session.getTransaction().rollback();
+			e.printStackTrace();
+		}
+		finally
+		{
+			session.close();
+		}
 	}
-
+	
 	@Override
-	public void deleteUser(long id) {
+	public void deleteUser(long id)
+	{
 		EntityManager em = HibernateUtil.getUserEntityManagerFactory().createEntityManager();
 		User user = em.find(User.class, id);
 		em.getTransaction().begin();
@@ -117,21 +203,36 @@ public class UserDaoImpl implements UserDao {
 		em.getTransaction().commit();
 		em.close();
 	}
-
+	
 	@Override
-	public void updateUserPassword(String userName, String newPassword) {
+	public void updateUserPassword(String userName, String newPassword)
+	{
 		Session session = HibernateUtil.getSessionFactory().openSession();
-		session.beginTransaction();
-		CriteriaBuilder cb = session.getCriteriaBuilder();
-		CriteriaUpdate<User> update = cb.createCriteriaUpdate(User.class);
-		Root<User> root = update.from(User.class);
-		update.set(root.get("password"), newPassword).where(cb.equal(root.get("userName"), userName));
-		session.createQuery(update).executeUpdate();
-		session.close();
+		try
+		{
+			session.beginTransaction();
+			CriteriaBuilder cb = session.getCriteriaBuilder();
+			CriteriaUpdate<User> update = cb.createCriteriaUpdate(User.class);
+			Root<User> root = update.from(User.class);
+			update.set(root.get(PASSWORD), newPassword).where(cb.equal(root.get("userName"), userName));
+			session.createQuery(update).executeUpdate();
+			session.getTransaction().commit();
+		}
+		catch(Exception e)
+		{
+			if(session.getTransaction() != null)
+				session.getTransaction().rollback();
+			e.printStackTrace();
+		}
+		finally
+		{
+			session.close();
+		}
 	}
-
+	
 	@Override
-	public void updateUserPassword(long id, String newPassword) {
+	public void updateUserPassword(long id, String newPassword)
+	{
 		EntityManager em = HibernateUtil.getUserEntityManagerFactory().createEntityManager();
 		User user = em.find(User.class, id);
 		em.getTransaction().begin();
@@ -139,23 +240,33 @@ public class UserDaoImpl implements UserDao {
 		em.getTransaction().commit();
 		em.close();
 	}
-
-	@Override
-	public void insertUser(String userName, String password, String email, Long id) {
-		Session session = HibernateUtil.getSessionFactory().openSession();
-		session.beginTransaction();
-	     
-	     User user = new User(userName, email, password, id);
-	     
-	     session.save(user);
-	     
-	     session.getTransaction().commit();
-	     session.close();
-	}
-
 	
 	@Override
-	public void insertUser(String username, String password, String email) {
+	public void insertUser(String userName, String password, String email, Long id)
+	{
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		try
+		{
+			session.beginTransaction();
+			User user = new User(userName, email, password, id);
+			session.save(user);
+			session.getTransaction().commit();
+		}
+		catch(Exception e)
+		{
+			if(session.getTransaction() != null)
+				session.getTransaction().rollback();
+			e.printStackTrace();
+		}
+		finally
+		{
+			session.close();
+		}
+	}
+	
+	@Override
+	public void insertUser(String username, String password, String email)
+	{
 		insertUser(username, password, email, JavaWebAppUtils.generateUniqueId());
 	}
 }
