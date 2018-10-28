a909ba2be4dc2141d1590f597fd67c6bdfe416e1
Evan <evan.rypel@gmail.com>
2018-10-28 13:26:30 -0700

    added a wallet. the plan is to give every user a wallet when they log in. I think I want this web app to serve as a 'digital asset bank' where users can view funds and move them around

--- src/com/javawebapp/Hello.java
+++ src/com/javawebapp/Hello.java
@@ -12,8 +12,10 @@ public class Hello
 		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 		HelloWorldService service = (HelloWorldService) context.getBean("helloWorldService");
+		WalletService wallet = (WalletService) context.getBean("walletService");
 		String message = service.sayHello();
-		System.out.println(message);
+		long walletId = wallet.getId();
+		System.out.println(message + " your wallet id is " + walletId + " and your public key is " + wallet.getPublicKey() + " and your private key is " + wallet.getPrivateKey());
 		
 		//set a new name
 		service.setName("Spring");
