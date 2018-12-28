32c0f329841128fad858bf4829f086d72bc307c5
Evan <evan.rypel@gmail.com>
2018-12-27 17:17:58 -0800

    implemented short HPC

--- src/com/javawebapp/service/HastyPuddingCipherService.java
+++ src/com/javawebapp/service/HastyPuddingCipherService.java
@@ -19,6 +19,7 @@ public class HastyPuddingCipherService
 	final static int bitSize = 64;
 	final int NUM_PASSES = 3; // number of passes for stirring function
 	final int NUM_WORDS = 256;
+	final BigInteger MOD = BigInteger.valueOf(2).pow(64); //applied to all addition, subtraction, multiplication
 	
 	// A few internal "random" numbers used in the cipher:
 	// Theoretically, BigInteger provides perfect accuracy
@@ -58,8 +59,8 @@ public class HastyPuddingCipherService
 		BigInteger KX[] = new BigInteger[NUM_WORDS + 30]; //256 words with wrap-around
 		
 		// The first 3 words are intialized as so:
-		KX[0] = PI19.add(BigInteger.valueOf(subCipherNumber));
-		KX[1] = E19.multiply(BigInteger.valueOf(keyLength));
+		KX[0] = PI19.add(BigInteger.valueOf(subCipherNumber)).mod(MOD);
+		KX[1] = E19.multiply(BigInteger.valueOf(keyLength)).mod(MOD);
 		// TODO should subCipherNumber be the number of bits instead?
 		KX[2] = shiftLeft(R220, subCipherNumber, bitSize);
 		
@@ -72,7 +73,7 @@ public class HastyPuddingCipherService
 		 */
 		for(int i = 3; i < NUM_WORDS; i++)
 		{
-			KX[i] = KX[i - 1].add(KX[i - 2].xor(KX[i - 3].shiftRight(23)).xor(shiftLeft(KX[i - 3], 41, bitSize)));
+			KX[i] = KX[i - 1].add(KX[i - 2].xor(KX[i - 3].shiftRight(23)).xor(shiftLeft(KX[i - 3], 41, bitSize))).mod(MOD);
 		}
 		
 		// TODO? I don't think we'll ever need this many words
@@ -134,28 +135,131 @@ public class HastyPuddingCipherService
 			{
 				// Perform the individual word stirring algorithm
 				// BigInteger is immutable so need to reassign values like this:
-				s0 = s0.xor((KX[i].xor(KX[(i + 83) & 255]).add(KX[s0.and(BigInteger.valueOf(255)).intValue()]))); // sometimes																							// lossy
-				s2 = s2.add(KX[i]); // necessary to prevent Wagner equivalent key problem
-				s1 = s1.add(s0);
+				s0 = s0.xor((KX[i].xor(KX[(i + 83) & 255]).add(KX[s0.and(BigInteger.valueOf(255)).intValue()]).mod(MOD))); // sometimes																							// lossy
+				s2 = s2.add(KX[i]).mod(MOD); // necessary to prevent Wagner equivalent key problem
+				s1 = s1.add(s0).mod(MOD);
 				s3 = s3.xor(s2);
-				s5 = s5.subtract(s4);
+				s5 = s5.subtract(s4).mod(MOD);
 				s7 = s7.xor(s6);
-				s3 = s3.add(s0.shiftRight(13));
+				s3 = s3.add(s0.shiftRight(13)).mod(MOD);
 				s4 = s4.xor(shiftLeft(s1, 11, bitSize));
 				s5 = s5.xor(shiftLeft(s3, s1.and(BigInteger.valueOf(31)).intValue(), bitSize));
-				s6 = s6.add(s2.shiftRight(17));
-				s7 = s7.or(s3.add(s4)); // lossy
-				s2 = s2.subtract(s5); // cross-link
-				s0 = s0.subtract(s6.xor(BigInteger.valueOf(i)));
-				s1 = s1.xor(s5.add(PI19));
-				s2 = s2.add(s7.shiftRight(j));
+				s6 = s6.add(s2.shiftRight(17)).mod(MOD);
+				s7 = s7.or(s3.add(s4).mod(MOD)); // lossy
+				s2 = s2.subtract(s5).mod(MOD); // cross-link
+				s0 = s0.subtract(s6.xor(BigInteger.valueOf(i))).mod(MOD);
+				s1 = s1.xor(s5.add(PI19).mod(MOD));
+				s2 = s2.add(s7.shiftRight(j)).mod(MOD);
 				s2 = s2.xor(s1);
-				s4 = s4.subtract(s3);
+				s4 = s4.subtract(s3).mod(MOD);
 				s6 = s6.xor(s5);
-				s0 = s0.add(s7);
-				KX[i] = s2.add(s6);
+				s0 = s0.add(s7).mod(MOD);
+				KX[i] = s2.add(s6).mod(MOD);
 			}
 		}
+	}
+	
+	public void HPCShort(Long plaintextDestTag, BigInteger[] KX)
+	{
+		int blocksize = 64;
+		/*
+		 * 	Permb was derived from the hex expansion of e (2.718...).  The
+		 *	fraction was grouped into chunks of 64 bits, and the first sixteen
+		 *	chunks with unique low-order 4bit hex digits were selected.  The
+		 *	twelfth and fourteenth entries would have been fixed points for the
+		 *	low-order 4 bits, so they were swapped.
+		 */
+		BigInteger[] permb = new BigInteger[] {
+			BigInteger.valueOf(0xB7E151628AED2A6Al),
+			BigInteger.valueOf(0xBF7158809CF4F3C7l),
+			BigInteger.valueOf(0x62E7160F38B4DA56l),
+			BigInteger.valueOf(0xA784D9045190CFEFl),
+			BigInteger.valueOf(0x324E7738926CFBE5l),
+			BigInteger.valueOf(0xF4BF8D8D8C31D763l),
+			BigInteger.valueOf(0xDA06C80ABB1185EBl),
+			BigInteger.valueOf(0x4F7C7B5757F59584l),
+			BigInteger.valueOf(0x90CFD47D7C19BB42l),
+			BigInteger.valueOf(0x158D9554F7B46BCEl),
+			BigInteger.valueOf(0x8A9A276BCFBFA1C8l),
+			BigInteger.valueOf(0xE5AB6ADD835FD1A0l),
+			BigInteger.valueOf(0x86D1BF275B9B241Dl),
+			BigInteger.valueOf(0xF0D3D37BE67008E1l),
+			BigInteger.valueOf(0x0FF8EC6D31BEB5CCl),
+			BigInteger.valueOf(0xEB64749A47DFDFB9l)
+		};
+		/*
+		 * 	The plaintext is placed right-justified in variable s0.  LMASK is set
+		 *	to a block of 1s, to mask s0 to the valid bits between operations.
+		 */
+		BigInteger s0 = BigInteger.valueOf(plaintextDestTag);
+		BigInteger lmask = BigInteger.valueOf(0xffffffffffffffffl);
+		
+		// A word from the KX array, KX[blocksize], is added to s0.
+		s0 = KX[blocksize].add(s0).mod(MOD);
+		s0.and(lmask);
+		
+		// Several shift sizes are calculated:
+		BigInteger LBH = BigInteger.valueOf((blocksize + 1) / 2); //division rounds down
+		BigInteger LBQ = (LBH.add(BigInteger.ONE)).divide(BigInteger.valueOf(2));
+		BigInteger LBT = (LBQ.add(BigInteger.valueOf(blocksize))).divide(BigInteger.valueOf(4)).add(BigInteger.valueOf(2));
+		BigInteger GAP = BigInteger.valueOf(64 - blocksize);
+		
+		/*
+		 * Then 8 rounds of mixing are run with round index i going from 0...7
+		 * After the mixing, another word from KX, KX[blocksize + 8] is added to s0
+		 * s0 is masked, and the valid bits are written to the output array.
+		 * Any high-order bits in the output array are unchanged.
+		 */
+		for(int i = 0; i < 8; i++)
+		{
+			BigInteger k = KX[s0.and(BigInteger.valueOf(255)).intValue()]; //+ spice[i]
+			k.and(lmask);
+			s0 = s0.add(k.shiftLeft(8)).mod(MOD);
+			s0 = s0.and(lmask);
+			s0 = s0.xor((k.shiftRight(GAP.intValue())).and(BigInteger.valueOf(~255)));
+			s0 = s0.and(lmask);
+			s0 = s0.add(s0.shiftLeft(LBH.intValue() + i)).mod(MOD);
+			s0 = s0.and(lmask);
+			//  t = spice[i^7]
+			// s0 ^= t
+			// s0 -= t>>(GAP+i)
+			// s0 += t>>13
+			s0 = s0.xor(s0.shiftRight(LBH.intValue()));
+			s0 = s0.and(lmask);
+			BigInteger t = s0.and(BigInteger.valueOf(255));
+			k = KX[t.intValue()];
+			//k ^= spice[i^4]
+			k.and(lmask);
+			k = KX[t.intValue()+3*i+1].add(k.shiftRight(23)).add(k.shiftLeft(41));
+			k = k.and(lmask);
+			s0 = s0.xor(k.shiftLeft(8));
+			s0 = s0.and(lmask);
+			s0 = s0.subtract(k.shiftRight(GAP.intValue()).and(BigInteger.valueOf(~255))).mod(MOD);
+			s0 = s0.and(lmask);
+			s0 = s0.subtract(s0.shiftLeft(LBH.intValue()));
+			s0 = s0.and(lmask);
+			//t = spice[i^1] ^ (PI19+blocksize)
+			s0 = s0.add(t.shiftLeft(3)).mod(MOD);
+			s0 = s0.and(lmask);
+			s0 = s0.xor(t.shiftRight(GAP.intValue()+2));
+			s0 = s0.and(lmask);
+			s0 = s0.subtract(t).mod(MOD);
+			s0 = s0.and(lmask);
+			s0 = s0.xor(s0.shiftRight(LBQ.intValue()));
+			s0 = s0.and(lmask);
+			s0 = s0.add(permb[s0.and(BigInteger.valueOf(15)).intValue()]).mod(MOD);
+			s0 = s0.and(lmask);
+			//t = spice[i^2]
+			s0 = s0.xor(t.shiftRight(GAP.intValue()+4));
+			s0 = s0.and(lmask);
+			s0 = s0.add(s0.shiftLeft(LBT.intValue() + s0.and(BigInteger.valueOf(15)).intValue())).mod(MOD);
+			s0 = s0.and(lmask);
+			s0 = s0.add(t).mod(MOD);
+			s0 = s0.and(lmask);
+			s0 = s0.xor(s0.shiftRight(LBH.intValue()));
+			s0 = s0.and(lmask);
+		}
+		
 	}
 	
 	/**
