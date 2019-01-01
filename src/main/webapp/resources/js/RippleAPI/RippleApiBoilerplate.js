77fcc1bbeb9e1c310d4eabcc3af0c2d3b6e1823f
Evan <evan.rypel@gmail.com>
2019-01-01 14:42:46 -0800

    adding RippleAPI for javascript. This way, we will be able to do stuff from both the front end and the backend and play around with everything to see which works best for which scenarios

---
+++ src/main/webapp/resources/js/RippleAPI/RippleApiBoilerplate.js
@@ -0,0 +1,30 @@
+/**
+ * boilerplate code to wrap custom code in RippleAPI
+ * 
+ * https://developers.ripple.com/rippleapi-reference.html#main_content_wrapper
+ */ 
+const RippleAPI = require('ripple-lib').RippleAPI;
+const api = new RippleAPI({
+	server: "wss://s.altnet.rippletest.net:51233" // test server
+});
+
+api.on('error', (errorCode, errorMessage) => {
+	  console.log(errorCode + ': ' + errorMessage);
+	});
+api.on('connected', () => {
+	  console.log('connected');
+	});
+
+api.on('disconnected', (code) => {
+	// code - [close
+	// code](https://developer.mozilla.org/en-US/docs/Web/API/CloseEvent) sent
+	// by the server
+	// will be 1000 if this was normal closure
+	console.log('disconnected, code:', code);
+	});
+	
+api.connect().then(() => {
+	  /* insert code here */
+	}).then(() => {
+	  return api.disconnect();
+	}).catch(console.error);
---
+++ src/main/webapp/resources/js/xrpWebsocketAPI/xrpWebsocket.js
@@ -0,0 +1,28 @@
+/**
+ *  Contains the web socket to interact with XRP ledger API found here:
+ *  https://developers.ripple.com/websocket-api-tool.html
+ */
+
+// Production Web Socket
+//let webSocket = new WebSocket("wss://s2.ripple.com:443 ");
+// TestNet web socket
+/**
+ * Test Credentials:
+ * 
+ * Public Address:
+ * rwYQjHp9HZiKKpZB4i4fvc8eQvAtA7vdY6
+ * 
+ * Private Key:
+ * snKixQChzs9KcBxxrYWpm97sxnA1e
+ */
+
+/**
+ * Second set of test credentials
+ * 
+ * Address 
+ * rntmtrrtSGS9dJD84krKvutJLeQ6mADgQp 
+ * Secret
+ * snzzPQHtaCt2oj6YEx5CmCLD3p9Qv 
+ * Balance 10,000 XRP
+ */
+const webSocket = new WebSocket("wss://s.altnet.rippletest.net:51233");
