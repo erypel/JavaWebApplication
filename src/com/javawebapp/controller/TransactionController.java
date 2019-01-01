1a352a2232ac5ca651b038245f4b0b841461f0e2
Evan <evan.rypel@gmail.com>
2018-12-31 21:22:10 -0800

    JSON is being created and modified, but we don't yet know if it is correct json for the rippleAPI. More testing to come!

--- src/com/javawebapp/controller/TransactionController.java
+++ src/com/javawebapp/controller/TransactionController.java
@@ -13,6 +13,7 @@ import com.javawebapp.model.objectsforrippleapi.Address;
 import com.javawebapp.model.objectsforrippleapi.Amount;
 import com.javawebapp.model.objectsforrippleapi.Instructions;
 import com.javawebapp.model.objectsforrippleapi.Payment;
+import com.javawebapp.model.objectsforrippleapi.Sequence;
 import com.javawebapp.model.objectsforrippleapi.Value;
 import com.javawebapp.service.TransactionService;
 
@@ -35,6 +36,8 @@ public class TransactionController
 		testPayment.setSourceAddress(new Address("rwYQjHp9HZiKKpZB4i4fvc8eQvAtA7vdY6"));
 		Instructions testInstr = new Instructions();
 		testInstr.setFee(new Value("1"));
+		testInstr.setSequence(new Sequence("rntmtrrtSGS9dJD84krKvutJLeQ6mADgQp"));
+		testInstr.setMaxLedgerVersionString(null);
 		JSONObject json = service.buildPaymentJson("rwYQjHp9HZiKKpZB4i4fvc8eQvAtA7vdY6", testPayment, testInstr);
 		return json;
 	}
